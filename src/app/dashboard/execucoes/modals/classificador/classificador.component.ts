901b8cfdb5309c09fff9e3e5cd040ac4687dc528
IanaMary <ianacosta1303@gmail.com>
2025-06-09 14:57:28 -0300

    adicionando a chave de hiperparametros.

--- src/app/dashboard/execucoes/modals/classificador/classificador.component.ts
+++ src/app/dashboard/execucoes/modals/classificador/classificador.component.ts
@@ -19,17 +19,7 @@ export class ClasificadorComponent implements OnChanges {
   ) { }
 
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('tipoClassficador ', this.modeloSelecionado)
-
-    const hiperparametros = this.modeloSelecionado?.hiperparametros?.reduce((obj, hp) => {
-      obj[hp.nomeHiperparametro] = hp.valorPadrao;
-      return obj;
-    }, {}) ?? {};
-
-
-    console.log('hiperparametros ', hiperparametros)
-  }
+  ngOnChanges(changes: SimpleChanges): void {}
 
   enviarParaClassificador() {
     const tipoClassficador = this.modeloSelecionado?.valor ?? '';
@@ -49,7 +39,6 @@ export class ClasificadorComponent implements OnChanges {
 
     this.dashboardService.classificadorTreino(tipoClassficador, body).subscribe({
       next: (res) => {
-        console.log('res, ', res)
         this.resultadoClassificador = res;
       },
       error: (err) => {
