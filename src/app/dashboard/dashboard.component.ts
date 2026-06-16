c30236b8e2d57d894e181786f8cfdce93c912867
ulissesbcorrea <ulissesbcorrea@gmail.com>
2026-06-16 03:36:15 -0300

    feat: mover botoes salvar/limpar para o header do pipeline
    - Botoes movidos do header de execucoes para o header do pipeline
    - Icone do botao limpar alterado de delete_outline para cleaning_services
    - Textos removidos, tooltips adicionados via matTooltip
    - Botoes alinhados a direita do titulo Pipeline

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DashboardService } from './services/dashboard.service';
+import { ExecucoesComponent } from './execucoes/execucoes.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +10,20 @@ import { DashboardService } from './services/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
 
+  @ViewChild(ExecucoesComponent) execucoesComponent!: ExecucoesComponent;
+
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
     this.dashboardService.carregarDados();
+  }
+
+  salvarPipeline() {
+    this.execucoesComponent?.salvarPipeline();
+  }
+
+  limparSessao() {
+    this.execucoesComponent?.limparSessao();
   }
 
 }
